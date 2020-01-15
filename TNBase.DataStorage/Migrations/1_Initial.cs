93031522bc42ef0335f0fb178612fd77ef086493
Audrius Asadauskas <audriusa83@gmail.com>
2020-01-15 22:10:36 +0000

    Added logig for creating SQL migrations

---
+++ TNBase.DataStorage/Migrations/1_Initial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace TNBase.DataStorage.Migrations
+{
+    public class _1_Initial : SqlMigration
+    {
+        public _1_Initial(SQLiteConnection connection) : base(connection)
+        { }
+
+        public override void Up()
+        {
+        }
+    }
+}
---
+++ TNBase.DataStorage/Migrations/SqlMigration.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+using System;
+using System.Data.SQLite;
+
+namespace TNBase.DataStorage.Migrations
+{
+    public abstract class SqlMigration
+    {
+        private Logger log = LogManager.GetCurrentClassLogger();
+        private readonly SQLiteConnection connection;
+
+        public SqlMigration(SQLiteConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        protected void Sql(string query)
+        {
+            log.Debug("Executing query: " + query);
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = query;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public abstract void Up();
+    }
+}
