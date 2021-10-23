dc320cbc087a0704552d1fc6a50f9333c762011f
Audrius <audriusa83@gmail.com>
2021-10-23 10:59:21 +0100

    Update product version

--- ConfigureVersion.ps1
+++ ConfigureVersion.ps1
@@ -3,8 +3,10 @@ param(
     [string]$version
 )
 
-$project = Get-Content .\TNBase.Setup.vdproj
-$project = $project.Replace('"ProductVersion" = "8:1.0.10"', '"ProductVersion" = "8:' + $version + '"')
+$path = .\TNBase.Setup\TNBase.Setup.vdproj
+$project = Get-Content $path
+
+$project = $project.Replace('"ProductVersion" = "8:1.0.0.0"', '"ProductVersion" = "8:' + $version + '"')
 
 $guid = New-Guid
 $productCode = '"ProductCode" = "8:{' + $guid.ToString().ToUpper()
@@ -14,5 +16,11 @@ $guid = New-Guid
 $packageCode = '"PackageCode" = "8:{' + $guid.ToString().ToUpper()
 $project = $project -replace '"PackageCode" = "8:{[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}', $packageCode
 
+Set-Content $path -Value $project
 
-Set-Content .\TNBase.Setup.vdproj -Value $project
+$path = .\TNBase\TNBase.csproj
+$project = Get-Content $path
+
+$project = $project.Replace("<Version>1.0.0.0</Version>", "<Version>$version</Version>")
+
+Set-Content $path -Value $project
