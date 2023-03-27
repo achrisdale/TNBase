03b7a89d240de4da857257529b5fc0529c47334d
Audrius <audriusa83@gmail.com>
2023-03-27 15:21:50 +0100

    Update FormSetDatabasePassword.Designer.cs

--- src/TNBase/Forms/FormSetDatabasePassword.Designer.cs
+++ src/TNBase/Forms/FormSetDatabasePassword.Designer.cs
@@ -48,10 +48,10 @@
             this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnCancel.Location = new System.Drawing.Point(50, 663);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnCancel.Location = new System.Drawing.Point(27, 311);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(378, 118);
+            this.btnCancel.Size = new System.Drawing.Size(204, 55);
             this.btnCancel.TabIndex = 34;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = false;
@@ -61,10 +61,10 @@
             this.btnSetPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.btnSetPassword.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.btnSetPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnSetPassword.Location = new System.Drawing.Point(750, 663);
-            this.btnSetPassword.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnSetPassword.Location = new System.Drawing.Point(404, 311);
+            this.btnSetPassword.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnSetPassword.Name = "btnSetPassword";
-            this.btnSetPassword.Size = new System.Drawing.Size(378, 118);
+            this.btnSetPassword.Size = new System.Drawing.Size(204, 55);
             this.btnSetPassword.TabIndex = 37;
             this.btnSetPassword.Text = "Set Password";
             this.btnSetPassword.UseVisualStyleBackColor = false;
@@ -73,9 +73,10 @@
             // tbxPassword
             // 
             this.tbxPassword.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbxPassword.Location = new System.Drawing.Point(432, 216);
+            this.tbxPassword.Location = new System.Drawing.Point(233, 101);
+            this.tbxPassword.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.tbxPassword.Name = "tbxPassword";
-            this.tbxPassword.Size = new System.Drawing.Size(696, 57);
+            this.tbxPassword.Size = new System.Drawing.Size(377, 32);
             this.tbxPassword.TabIndex = 38;
             this.tbxPassword.UseSystemPasswordChar = true;
             this.tbxPassword.TextChanged += new System.EventHandler(this.tbxPassword_TextChanged);
@@ -84,9 +85,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(50, 212);
+            this.label1.Location = new System.Drawing.Point(27, 99);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(211, 59);
+            this.label1.Size = new System.Drawing.Size(108, 30);
             this.label1.TabIndex = 39;
             this.label1.Text = "Password:";
             // 
@@ -94,18 +96,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(50, 368);
+            this.label2.Location = new System.Drawing.Point(27, 172);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(376, 59);
+            this.label2.Size = new System.Drawing.Size(194, 30);
             this.label2.TabIndex = 41;
             this.label2.Text = "Confirm password:";
             // 
             // tbxConfimPassword
             // 
             this.tbxConfimPassword.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbxConfimPassword.Location = new System.Drawing.Point(432, 372);
+            this.tbxConfimPassword.Location = new System.Drawing.Point(233, 174);
+            this.tbxConfimPassword.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.tbxConfimPassword.Name = "tbxConfimPassword";
-            this.tbxConfimPassword.Size = new System.Drawing.Size(696, 57);
+            this.tbxConfimPassword.Size = new System.Drawing.Size(377, 32);
             this.tbxConfimPassword.TabIndex = 40;
             this.tbxConfimPassword.UseSystemPasswordChar = true;
             this.tbxConfimPassword.Validating += new System.ComponentModel.CancelEventHandler(this.tbxConfirmPassword_Validating);
@@ -114,9 +118,10 @@
             // 
             this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.Color.Firebrick;
-            this.label3.Location = new System.Drawing.Point(50, 41);
+            this.label3.Location = new System.Drawing.Point(27, 19);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(1030, 152);
+            this.label3.Size = new System.Drawing.Size(555, 71);
             this.label3.TabIndex = 42;
             this.label3.Text = "Important! Data cannot be recovered if password is lost.";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -126,18 +131,20 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label4.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.label4.Location = new System.Drawing.Point(445, 273);
+            this.label4.Location = new System.Drawing.Point(235, 135);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(576, 50);
+            this.label4.Size = new System.Drawing.Size(296, 25);
             this.label4.TabIndex = 43;
             this.label4.Text = "Leave blank to remove encryption";
             // 
             // lblPasswordStrength
             // 
             this.lblPasswordStrength.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblPasswordStrength.Location = new System.Drawing.Point(90, 497);
+            this.lblPasswordStrength.Location = new System.Drawing.Point(48, 233);
+            this.lblPasswordStrength.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPasswordStrength.Name = "lblPasswordStrength";
-            this.lblPasswordStrength.Size = new System.Drawing.Size(995, 139);
+            this.lblPasswordStrength.Size = new System.Drawing.Size(536, 65);
             this.lblPasswordStrength.TabIndex = 44;
             this.lblPasswordStrength.Text = "Password strength";
             this.lblPasswordStrength.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -150,11 +157,11 @@
             // FormSetDatabasePassword
             // 
             this.AcceptButton = this.btnSetPassword;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(1182, 836);
+            this.ClientSize = new System.Drawing.Size(636, 392);
             this.Controls.Add(this.lblPasswordStrength);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -166,6 +173,7 @@
             this.Controls.Add(this.btnCancel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.Name = "FormSetDatabasePassword";
             this.Text = "Set Database Password";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormSetDatabasePassword_FormClosing);
