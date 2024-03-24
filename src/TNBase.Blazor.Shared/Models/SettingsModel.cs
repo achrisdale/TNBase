98328bed05f699da70b43566e1b373089fcf36d9
Audrius <audriusa83@gmail.com>
2024-03-24 14:02:30 +0000

    Delete SettingsModel.cs

--- src/TNBase.Blazor.Shared/Models/SettingsModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TNBase.Blazor.Shared.Models
-{
-    public class SettingsModel
-    {
-        public int Wallet { get; set; }
-        public string Forename { get; set; }
-        public string Surname { get; set; }
-    }
-}
